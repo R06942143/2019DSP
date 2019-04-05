7df35e210cff27f5a594ebcdcef1040960546d62
conrad <r06942143@ntu.edu.tw>
2019-04-05 23:38:28 +0800

    hw

--- dsp_hw1/c_cpp/test_hmm.c
+++ dsp_hw1/c_cpp/test_hmm.c
@@ -1,17 +1,63 @@
 #include "hmm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-int main()
-{
-/*
-	HMM hmms[5];
-	load_models( "modellist.txt", hmms, 5);
-	dump_models( hmms, 5);
-*/
-	HMM hmm_initial;
-	loadHMM( &hmm_initial, "model_init.txt" );
-	dumpHMM( stderr, &hmm_initial );
+double p[MAX_SEQ][MAX_STATE];
+double Pro[5];
 
-	printf("%f\n", log(1.5) ); // make sure the math library is included
+int main(int argc,char *argv[])
+{
+	HMM hmmdata[5];
+	//test
+	int N,Len,target,i,j,n,t;
+	char line_data[256];
+	double max_pro;
+	//accurancy
+	int correct,total;
+	char line_model[256],tmp[256],pp[30];
+	
+	load_models( argv[1], hmmdata, 5);
+	
+	FILE *input=open_or_die(argv[2],"r");  
+	FILE *output=open_or_die(argv[3],"w+");
+
+	while(fscanf(input,"%s",line_data)>0)
+	{
+		Len=strlen(line_data);
+		for(n=0;n<5;n++)
+		{
+			N=hmmdata[n].state_num;
+
+			for(i=0;i<N;i++)	//initial
+				p[0][i]=hmmdata[n].initial[i]*hmmdata[n].observation[line_data[0]-'A'][i];
+
+			for(t=1;t<Len;t++)	//induction
+			{
+				for(j=0;j<N;j++)
+				{
+					max_pro=0.0;
+					for(i=0;i<N;i++)
+						if(p[t-1][i]*hmmdata[n].transition[i][j] > max_pro)
+							max_pro=p[t-1][i]*hmmdata[n].transition[i][j];
+					p[t][j]=max_pro*hmmdata[n].observation[line_data[t]-'A'][j];
+				}
+			}
+			max_pro=0.0;
+			for(i=0;i<N;i++)
+				if(p[Len-1][i] > max_pro)
+					max_pro=p[Len-1][i];
+			Pro[n]=max_pro;
+		}
+		max_pro=0.0; 
+		for(n=0;n<5;n++)
+			if(Pro[n]>max_pro)
+			{
+				max_pro=Pro[n];
+				target=n;
+			}	
+		fprintf(output,"%s %e\n",hmmdata[target].model_name,max_pro);
+	}
 	return 0;
 }
