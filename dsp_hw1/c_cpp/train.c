ea5000a115e67fa56bf2baa085246a9eac5fdd9b
conrad <r06942143@ntu.edu.tw>
2019-04-06 13:11:40 +0800

    hw

--- dsp_hw1/c_cpp/train.c
+++ dsp_hw1/c_cpp/train.c
@@ -86,6 +86,7 @@ int main(int argc, char* argv[]){
                 }
             }
         }
+        clearerr(f_seq);
         printf("%d",N);
         for(int s=0;s<state_num;s++){
             //update pi
