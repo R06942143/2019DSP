c8b3ceacd0b3d8926a5e5e077136ea4844ec3bfa
conrad <r06942143@ntu.edu.tw>
2019-04-06 17:23:31 +0800

    hw

--- dsp_hw1/c_cpp/test.c
+++ dsp_hw1/c_cpp/test.c
@@ -7,56 +7,47 @@
 double p[MAX_SEQ][MAX_STATE];
 double Pro[5];
 
-int main(int argc,char *argv[])
+void main(int argc,char *argv[])
 {
     HMM hmmdata[5];
 	load_models( argv[1], hmmdata, 5);
 	FILE *input=open_or_die(argv[2],"r");  
-	FILE *output=open_or_die(argv[3],"w+");
-
-	//test
-	int N,Len,target,i,j,n,t;
+	FILE *result=open_or_die(argv[3],"w+");
+	
 	char seq[256];
-	double max_pro;
-	//accurancy
-	int correct,total;
-	char line_model[256],tmp[256],pp[30];
 
 	while(fscanf(input,"%s",seq)>0)
 	{
-		Len=strlen(seq);
-		for(n=0;n<5;n++)
+		int T = strlen(seq);
+		double max_ = 0.0;
+		int num_max = 0;
+		for(int n=0;n<5;n++)
 		{
-			N=hmmdata[n].state_num;
-
-			for(i=0;i<N;i++)	
+			int N=hmmdata[n].state_num;
+			//initial
+			for(int i=0;i<N;i++)	
 				p[0][i]=hmmdata[n].initial[i]*hmmdata[n].observation[seq[0]-'A'][i];
 
-			for(t=1;t<Len;t++)	//induction
+			//recursion
+			for(int t=1;t<T;t++)
 			{
-				for(j=0;j<N;j++)
+				for(int j=0;j<N;j++)
 				{
-					max_pro=0.0;
+					p[t][j] = 0.0;
+					//find max seqence
 					for(i=0;i<N;i++)
-						if(p[t-1][i]*hmmdata[n].transition[i][j] > max_pro)
-							max_pro=p[t-1][i]*hmmdata[n].transition[i][j];
-					p[t][j]=max_pro*hmmdata[n].observation[seq[t]-'A'][j];
+						p[t][j]=max(p[t][j],p[t-1][i]*hmmdata[n].transition[i][j]);
+					p[t][j] *= hmmdata[n].observation[seq[t]-'A'][j];
 				}
 			}
-			max_pro=0.0;
-			for(i=0;i<N;i++)
-				if(p[Len-1][i] > max_pro)
-					max_pro=p[Len-1][i];
-			Pro[n]=max_pro;
+			
+			for(i=0;i<N;i++){
+				if(p[T-1][i]>max_){
+					max_ = p[T-1][i];
+					num_max=n;
+				}
+			}
 		}
-		max_pro=0.0; 
-		for(n=0;n<5;n++)
-			if(Pro[n]>max_pro)
-			{
-				max_pro=Pro[n];
-				target=n;
-			}	
-		fprintf(output,"%s %e\n",hmmdata[target].model_name,max_pro);
+		fprintf(result,"%s %e\n",hmmdata[num_max].model_name,max_);
 	}
-	return 0;
 }
